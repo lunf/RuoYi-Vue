7e3bc5a056cadcf8c3f12d4ca35e6c3376e15510
Cuong Nguyen <nhatcuong.nguyen@vmodev.com>
2020-12-24 17:12:14 +0700

    Translate index, errorCode and Navbar

--- ruoyi-ui/src/utils/errorCode.js
+++ ruoyi-ui/src/utils/errorCode.js
@@ -1,6 +1,6 @@
 export default {
-  '401': '认证失败，无法访问系统资源',
-  '403': '当前操作没有权限',
-  '404': '访问资源不存在',
-  'default': '系统未知错误，请反馈给管理员'
+  '401': 'Authentication failed, unable to access system resources',
+  '403': 'No permission for current operation',
+  '404': 'Access resource does not exist',
+  'default': 'System unknown error, please report to the administrator'
 }
